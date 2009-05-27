6b7db68401dff5e94670582638f592aee8a58aa1
joachim <joachim@haagen.name>
2009-05-27 13:06:12 +0000

    Refactoring and cleanup. Changed version in POM.XML to 0.2.0

--- JSFlot/trunk/src/main/java/org/jsflot/components/JSFlotAjaxInterface.java
+++
@@ -1,8 +0,0 @@
-package org.jsflot.components;
-
-import javax.faces.context.FacesContext;
-
-public interface JSFlotAjaxInterface {
-	public void handleAjaxRequest(FacesContext context, JSFlotAjaxInterface component);
-	public void handleAjaxDecode(FacesContext context, JSFlotAjaxInterface component);
-}
